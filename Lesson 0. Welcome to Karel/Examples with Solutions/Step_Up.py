cdbea2374ac1da802f7e67503fdced8afc49d2ae
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-03 21:00:17 +0100

    Step_Up

---
+++ Lesson 0. Welcome to Karel/Examples with Solutions/Step_Up.py
@@ -0,0 +1,26 @@
+# This tells python who Karel is!
+# Every Karel file has a line just like it
+from karel.stanfordkarel import *
+
+# this program executes in a special function called main
+def main():
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    put_beeper()
+    move()
+
+# the definition of turn_right (Karel turn right by turning left three times)
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# This is "boilerplate" code which launches your code
+# when you hit the run button
+if __name__ == '__main__':
+    main()
