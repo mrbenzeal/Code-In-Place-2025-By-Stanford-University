dbaabda598c0c087e04834409c9cb486622c9403
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-03 22:26:43 +0100

    Midpoint_Karel

---
+++ Lesson 3. Art of Problem Solving/Coding Challenges/2-Midpoint_Karel.py
@@ -0,0 +1,19 @@
+# This tells Python who Karel is
+# Every Karel file has a line just like it
+from karel.stanfordkarel import *
+
+"""
+
+"""
+def main():
+    
+    
+
+
+"""
+This is a boilerplate code which launches our code
+when we hit the run button.
+# There is no need to edit code beyond this point
+"""
+if __name__ == '__main__':
+    main()
