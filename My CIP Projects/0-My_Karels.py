2dbbeb0b1c6031dba18a28379d6e74df183db82b
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-03 22:44:35 +0100

    My_Karels

---
+++ My CIP Projects/0-My_Karels.py
@@ -0,0 +1,18 @@
+# This tells Python who Karel is
+# Every Karel file has a line just like it
+from karel.stanfordkarel import *
+
+"""
+"""
+def main():
+
+
+
+
+
+"""
+This is a boilerplate code which launches our code
+when we hit the run button.
+"""
+if __name__ == '__main__':
+    main()
