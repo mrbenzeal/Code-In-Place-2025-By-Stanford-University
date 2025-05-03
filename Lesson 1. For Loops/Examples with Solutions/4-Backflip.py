ae558e234a4be86c2e6abb065695bdbe8374ec7e
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-03 21:30:28 +0100

    Backflip

---
+++ Lesson 1. For Loops/Examples with Solutions/4-Backflip.py
@@ -0,0 +1,20 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+"""
+
+from karel.stanfordkarel import *
+
+def main():
+    """
+    Makeing Karel do a sick backflip by turning left 4 times.
+    """
+    for i in range(4):
+        turn_left()
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
