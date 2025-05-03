31b8efa75007ba531beb8f71455ad2a4ad9bd109
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-03 22:19:35 +0100

    Follow_Trail

---
+++ Lesson 3. Art of Problem Solving/Examples with Solutions/7-Follow_Trail.py
@@ -0,0 +1,51 @@
+# This tells Python who Karel is
+# Every Karel file has a line just like it
+from karel.stanfordkarel import *
+
+"""
+This code instructs Karel to follow the trail of beepers 
+(picking up beepers as she goes) until the end!
+Karel will end up in the third cell down from the top right corner, facing East.
+"""
+
+def main():
+    while beepers_present():
+        follow_straight_trail()
+        step_backwards()
+        # Check left
+        turn_left()
+        move()
+        if no_beepers_present():
+            # Trail doesn't continue to the left;
+            # Go right
+            step_backwards()
+            turn_around()
+            move()
+            # Here the next iteration of the loop will check if there is a beeper; if there is we will keep going and if not we will stop!
+
+
+def follow_straight_trail():
+    while beepers_present():
+        pick_beeper()
+        move()
+
+
+def step_backwards():
+    """
+    Turn around and go back one step, 
+    then face the same way you were when you started
+    """
+    turn_around()
+    move()
+    turn_around()
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+
+# This is "boilerplate" code which launches your code
+# when you hit the run button
+if __name__ == '__main__':
+    main()
