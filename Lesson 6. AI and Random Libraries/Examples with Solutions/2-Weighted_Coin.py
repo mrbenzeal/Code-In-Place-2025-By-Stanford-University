b81a13612e96e7acfaf0d0a15826f12c8ba12da7
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-08 18:08:56 +0100

    Weighted_Coin

---
+++ Lesson 6. AI and Random Libraries/Examples with Solutions/2-Weighted_Coin.py
@@ -0,0 +1,28 @@
+"""
+File: Weighted_Coin.py
+------------------
+Simulate flip a weighted coin. this  is a coin where the probability of heads isn't 50%.
+This program uses the random module to "flip" coin where the probability of heads is 70% 
+and print the outcome (heads or tails).
+"""
+
+# Import the random library which lets us simulate random things like dice!
+import random
+
+"""
+This main function definition serves as the entry point of the program 
+and is responsible for executing the primary functionality 
+of printing to the console.
+"""
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == '__main__':
+    main()
