3518b4020c08ad01f34e7969d4cc1652ae864b9e
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-08 10:42:52 +0100

    Mars_Weight

---
+++ CIP_Section_5 Assignments/CIP5_S3a-Mars_Weight.py
@@ -0,0 +1,27 @@
+"""
+# Section 3: Console Programming
+# Program: Mars_Weight.py
+----------------------------------
+Prompts the user for a weight on Earth
+and prints the equivalent weight on Mars.
+"""
+
+"""
+This main function definition serves as the entry point of the program 
+and is responsible for executing the primary functionality 
+of printing to the console.
+"""
+def main():
+    # Fill this function out!
+    pass
+
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == "__main__":
+    main()
