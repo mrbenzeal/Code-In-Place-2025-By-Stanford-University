0b100c9000ee44b6af58fbc187ff09767013f456
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-08 18:30:57 +0100

    N-Sided_Dice

---
+++ Lesson 6. AI and Random Libraries/Coding Challenges/0-N-Sided_Dice.py
@@ -0,0 +1,28 @@
+"""
+File: N-Sided_Dice.py
+------------------
+This is a program which takes as input the number of sides on a dice. 
+Then, simulates rolling a dice with that many sides. 
+And prints the outcome of the roll.
+"""
+
+# Import the random library which lets us simulate random things like dice!
+import random
+
+"""
+This main function definition serves as the entry point of the program 
+and is responsible for executing the primary functionality 
+of printing to the console.
+"""
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == '__main__':
+    main()
