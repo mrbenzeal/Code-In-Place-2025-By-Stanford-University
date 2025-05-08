2d2fcbb3b5f2d51b8ab6a5493a983d39e5f44bcb
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-08 10:44:19 +0100

    Planetary_Weight_Calculator

---
+++ CIP_Section_5 Assignments/CIP5_S3b-Planetary_Weight_Calculator.py
@@ -0,0 +1,34 @@
+"""
+# Section 3: Console Programming
+# Program: Planetary_Weight_Calculator.py
+------------------------------------------
+Prompts the user for a weight on Earth
+and a planet (in separate inputs). Then 
+prints the equivalent weight on that planet.
+
+Note that the user should type in a planet with 
+the first letter as uppercase, and you do not need
+to handle the case where a user types in something 
+other than one of the planets (that is not Earth). 
+"""
+
+
+"""
+This main function definition serves as the entry point of the program 
+and is responsible for executing the primary functionality 
+of printing to the console.
+"""
+def main():
+    # Fill this function out!
+    pass
+
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == "__main__":
+    main()
