98850cbc777e5310fef7b1c4ef4c187071c4bea7
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-09 11:12:49 +0100

    Mars_Weight

--- CIP_Section_5 Assignments/CIP5_S3a-Mars_Weight.py
+++ CIP_Section_5 Assignments/CIP5_S3a-Mars_Weight.py
@@ -6,15 +6,29 @@ Prompts the user for a weight on Earth
 and prints the equivalent weight on Mars.
 """
 
+# using the constant (i.e, the weight ratio of the earth & mars: 37.8%)
+EARTH_TO_MARS_WEIGHT_RATIO = 0.378
+
 """
 This main function definition serves as the entry point of the program 
 and is responsible for executing the primary functionality 
 of printing to the console.
 """
 def main():
-    # Fill this function out!
-    pass
+    # Takes input from user as a string (Earthling weight on Earth)
+    earth_weight_string = input("Enter a weight on Earth: ")
+    
+    # Casting string into a real number (float)
+    earth_weight_float = float(earth_weight_string)
+    
+    # Calculating Earthling weight on Mars
+    mars_weight = earth_weight_float * EARTH_TO_MARS_WEIGHT_RATIO
 
+    # Rounded to two decimal places
+    rounded_mars_weight = round(mars_weight, 2)
+    
+    # converting the Earthling weight on mars to string and Print it using f-string   
+    print(f"The equivalent weight on Mars: {str(rounded_mars_weight)}")
 
 
 """
