ea762ffadffff28ebe4da72aa65021a3eac9cb41
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-12 23:18:17 +0100

    Weighted_Coin

--- Lesson 6. AI and Random Libraries/Examples with Solutions/2-Weighted_Coin.py
+++ Lesson 6. AI and Random Libraries/Examples with Solutions/2-Weighted_Coin.py
@@ -6,11 +6,10 @@ This program uses the random module to "flip" coin where the probability of head
 and print the outcome (heads or tails).
 """
 
-# Import the random library which lets us simulate random things like dice!
+# Import the random library which lets us simulate the coin flip!
 import random
 
-# define CONSTANT
-HEAD_PROBABILITY = 0.7 # 70% chance of heads
+PROBABILITY_OF_HEADS = 0.70 # 70% chance of heads
 
 """
 This main function definition serves as the entry point of the program 
@@ -18,34 +17,14 @@ and is responsible for executing the primary functionality
 of printing to the console.
 """
 def main():
-    flip = random.randint(0,1)
-    if flip < HEAD_PROBABILITY:
-        print("Heads")
-    else:
-        print("Tails")
+    # geting the probability
+    random_draw = random.random() # random.random() returns a random floating num between 0 and 1
 
-"""
-def main():
-    if random.random() < HEADS_WEIGHT:  # random.random() returns a random floating num between 0 and 1
+    if random_draw < PROBABILITY_OF_HEADS: 
         print("Heads!")  # 70% of heads
     else:
         print("Tails!")  # 30% of tails
-"""
-
-"""
-def main():
-    #for i in range(20):
-    #    print(f'Coin flip {i} is: {flip_coin()}')
-    print('Coin is flipped....\n...\n...')
-    print(f'The result is {flip_coin()}')
-
-def flip_coin()->str:
-    toss = random.randint(1,10)
-    if toss < 8:
-        return 'heads'
-    else:
-        return 'tails'
-"""
+        
 
 """
 # This provided line is required at the end of Python file 
