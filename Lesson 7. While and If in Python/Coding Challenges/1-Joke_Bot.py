9ff09a96edea457888b361395cab8c7e895b6751
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-14 23:34:06 +0100

    Joke_Bot

---
+++ Lesson 7. While and If in Python/Coding Challenges/1-Joke_Bot.py
@@ -0,0 +1,43 @@
+"""
+File: Joke_Bot.py
+-------------------
+This is a simple joke bot. This bot starts by asking the user what they want. 
+However, this program will only respond to one response: "Joke". 
+If the user enters "Joke" then it will print out a single joke. 
+Each time the joke is always the same.
+"""
+
+PROMPT = "What do you want? "
+JOKE = "Here is a joke for you! Karel is heading out to the grocery store. A programmer tells her: get a liter of milk, and if they have eggs, get 12. Karel returns with 13 liters of milk. The programmer asks why and Karel replies: 'because they had eggs'"
+SORRY = "Sorry I only tell jokes"
+
+"""
+This main function definition serves as the entry point of the program 
+and is responsible for executing the primary functionality such as managing user input, 
+doubling of the user entered number if it is less than 100 untill is 100 or greater
+and printing to the console.
+"""
+def main():
+    # Capture user input
+    user_input = input(PROMPT)
+    
+    # Input for string comparison
+    if user_input == "Joke":
+        print(JOKE)
+    elif str.upper(user_input) == "JOKE": # Uppercase input for string comparison
+        print(JOKE)
+    elif str.lower(user_input) == "joke": # Lowercase input for string comparison
+        print(JOKE)
+    else:
+        print(SORRY)
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == "__main__":
+    main()
+  
