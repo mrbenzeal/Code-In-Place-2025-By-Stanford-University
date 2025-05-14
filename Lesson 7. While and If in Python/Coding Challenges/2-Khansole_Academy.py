289e92262f4c1cbc14276f32762deedf8ed9bd1f
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-14 23:48:24 +0100

    Khansole_Academy

---
+++ Lesson 7. While and If in Python/Coding Challenges/2-Khansole_Academy.py
@@ -0,0 +1,28 @@
+"""
+File: Khansole_Academy.py
+--------------------------
+This is a program which prompts the user to enter a number; 
+the program will then double that number and print out the result. 
+It will repeat that process until the value is 100 or greater. 
+"""
+
+"""
+This main function definition serves as the entry point of the program 
+and is responsible for executing the primary functionality such as managing user input, 
+doubling of the user entered number if it is less than 100 untill is 100 or greater
+and printing to the console.
+"""
+def main():
+    print("Khansole Academy")
+    # TODO: your code here
+    
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == '__main__':
+    main()
+  
