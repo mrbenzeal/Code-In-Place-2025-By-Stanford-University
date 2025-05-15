b3d4cbc4e44d172ef8b36071d3c23ded17b12109
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-15 01:20:05 +0100

    Khansole_Academy

--- Lesson 7. While and If in Python/Coding Challenges/2-Khansole_Academy.py
+++ Lesson 7. While and If in Python/Coding Challenges/2-Khansole_Academy.py
@@ -1,20 +1,33 @@
 """
 File: Khansole_Academy.py
 --------------------------
-This is a program which prompts the user to enter a number; 
-the program will then double that number and print out the result. 
-It will repeat that process until the value is 100 or greater. 
+This program randomly generates simple addition problems for the user,
+(2-digit integers (i.e., the numbers 10 through 99))
+reads in the answer from the user, and then checks to see
+if they got it right or wrong, until the user appears to
+have got problem correct.
+The program keeps giving the user problems until the user get problem correct.
 """
 
-"""
-This main function definition serves as the entry point of the program 
-and is responsible for executing the primary functionality such as managing user input, 
-doubling of the user entered number if it is less than 100 untill is 100 or greater
-and printing to the console.
-"""
+import random 
+
 def main():
+    # Initialise the Khansole
     print("Khansole Academy")
-    # TODO: your code here
+    # Calculate two random number 
+    num1 = random.randint(10, 99)
+    num2 = random.randint(10, 99)
+    # Print two random numbers between 10 and 99 as an adition
+    print(f"What is {num1} + {num2}?")
+    # Ask the user type the result
+    answer = input("Your answer: ")
+    result = int(num1) + int(num2)
+    # If the answer type for th user is correct show a message "correct!"
+    if int(answer) == result:
+        print("Correct!")
+    else:
+        print(f"Incorrect.\nThe expected answer is {result}")
+    # If the answer type for the user is incorrect, print the value of the expected value
     
 
 """
