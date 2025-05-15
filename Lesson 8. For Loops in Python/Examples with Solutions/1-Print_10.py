fcd4c006761390b088f213d98391c939c0f7bc91
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-15 01:41:26 +0100

    Print_10

---
+++ Lesson 8. For Loops in Python/Examples with Solutions/1-Print_10.py
@@ -0,0 +1,13 @@
+"""
+File: Print_10.py
+------------------
+This is a program that prints the first 10 numbers in order using a for loop.
+"""
+
+
+def main():
+    print("Your code here")
+
+if __name__ == '__main__':
+    main()
+  
