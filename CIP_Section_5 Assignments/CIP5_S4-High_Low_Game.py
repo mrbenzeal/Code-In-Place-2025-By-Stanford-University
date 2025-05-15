3fbe855e22bb7904168f594cbce8573e8dedbae0
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-15 14:20:43 +0100

    High_Low_Game

--- CIP_Section_5 Assignments/CIP5_S4-High_Low_Game.py
+++ CIP_Section_5 Assignments/CIP5_S4-High_Low_Game.py
@@ -1,3 +1,10 @@
+"""
+File: High_Low_Game.py
+-----------------------
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+
 import random
 
 NUM_ROUNDS = 5
@@ -8,5 +15,12 @@ def main():
     
     # TODO: Write your code here!!! :)
 
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == "__main__":
     main()
