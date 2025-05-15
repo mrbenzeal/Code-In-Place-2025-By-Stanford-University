67affe219fc519037b5360f18ebfa609a0287ef5
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-15 01:37:16 +0100

    Even_Numbers

---
+++ Lesson 8. For Loops in Python/Examples with Solutions/0-Even_Numbers.py
@@ -0,0 +1,12 @@
+"""
+This is a program that prints the first 20 even numbers.
+"""
+
+def main():
+    # This for-loop start at 0 and counts up to 19 (for a total of 20 numbers)
+    for i in range(20):
+        print(i * 2)  # Use the 'i' value inside the for-loop
+   
+# Call the main function when "run", no need to edit anything below!
+if __name__ == "__main__":
+    main()
