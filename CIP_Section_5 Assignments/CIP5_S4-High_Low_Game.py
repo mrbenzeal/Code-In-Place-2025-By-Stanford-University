eefb1b9465b6c490cf49b18a6b52f7ad1c5d9dec
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-16 01:20:07 +0100

    High_Low_Game

--- CIP_Section_5 Assignments/CIP5_S4-High_Low_Game.py
+++ CIP_Section_5 Assignments/CIP5_S4-High_Low_Game.py
@@ -1,15 +1,15 @@
 """
 File: High_Low_Game.py
 -----------------------
-This program lets a user play a High-Low game with the computer.
+This program lets a player play a High-Low game with the computer.
 The game is called High-Low and the way it's played goes as follows:
 1. Two numbers are generated from 1 to 100 (inclusive on both ends): 
-one for the user and one for a computer, who will be the user's opponent. 
-The user gets to see his number, but not the computer's!
-2. The user make a guess, saying his number is either 
+one for the player and one for a computer, who will be the player's opponent. 
+The player gets to see his number, but not the computer's!
+2. The player make a guess, saying his number is either 
 higher than or lower than the computer's number
-3. If the user's guess matches the truth (ex. he guess his number is higher, 
-and then his number is actually higher than the computer's), the user gets a point!
+3. If the player's guess matches the truth (ex. he guess his number is higher, 
+and then his number is actually higher than the computer's), the player gets a point!
 
 These steps make up one round of the game. 
 The game is over after all rounds of 5 have been played.
@@ -20,23 +20,28 @@ import random
 # The number of rounds constant (the game is over after all rounds of 5)
 NUM_ROUNDS = 5
 
-
+"""
+The main function prints the name of the game to console, 
+declares the round count and the player's score as global variables 
+and calls the function Play_High_Low_Game() to start the game
+"""
 def main():
     print("Welcome to the High-Low Game!")
     print('--------------------------------')
 
-    # Declaring the round count and the user's score as global variables
+    # Declaring the round count and the player's score as global variables
     global round_count
-    global your_score
+    global player_score
     
     # Play the High or Low game
     Play_High_Low_Game()
 
 
+# This function starts the High Low Game
 def Play_High_Low_Game():
-    # Initialise the round count and the user's score
+    # Initialise the round count and the player's score
     round_count = 1
-    your_score = 0
+    player_score = 0
 
     # iterate through the number of rounds to play the game
     for i in range (NUM_ROUNDS):
@@ -47,31 +52,39 @@ def Play_High_Low_Game():
             print(f"Round {round_count}")
             round_count += 1
 
-            # Generate the user's and the computer's random numbers
-            your_random_number = random.randint(1,100)
+            # Generate the player's and the computer's random numbers
+            player_number = random.randint(1,100)
             computer_random_number = random.randint(1,100)
             
-            # Print the user's random numbers to console
-            print(f"Your number is {your_random_number}")
+            # Print the player's random numbers to console
+            print(f"Your number is {player_number}")
 
-            # Capture user's choise of either higher or lower
-            your_choice = input("Do you think your number is higher or lower than the computer's?: ")
+            # Capture player's guess of either 'higher' or 'lower' than the computer's number
+            player_guess = input("Do you think your number is higher or lower than the computer's?: ")
 
+            # This 'if condition' is for the player's guess to be in lower case  
+            if player_guess.lower() == 'higher' or player_guess.lower() == 'lower':
+                
+                # This captures the 2 possible true guesses of the player
+                is_lower_correct = player_guess.lower() == 'lower' and player_number < computer_random_number
+                is_higher_correct = player_guess.lower() == 'higher' and player_number > computer_random_number
 
-            if your_choice.lower() == 'higher' or your_choice.lower() == 'lower':
-                true_guess_1 = your_choice.lower() == 'lower' and your_random_number < computer_random_number
-                true_guess_2 = your_choice.lower() == 'higher' and your_random_number > computer_random_number
+                # The 'if condition' for the player's guess to be correct
+                if is_lower_correct or is_higher_correct: 
 
-                if true_guess_1 or true_guess_2: # correct
+                    # this captures what happens if the player guesses right
                     print(f"You were right! The computer's number was {computer_random_number}")
-                    your_score += 1
-                    print(f"Your score is now {your_score}")
+                    
+                    player_score += 1 # Increment number of player's score by 1
+                    print(f"Your score is now {player_score}")
 
-                else: # incorrect
+                # this captures what happens if the player fails to guess right
+                else: 
                     print(f"Aww, that's incorrect. The computer's number was {computer_random_number}")
-                    print(f"Your score is now {your_score}")
-
-            else:
+                    print(f"Your score is now {player_score}")
+            
+            # this captures what happens if the player inputs an invalid responds
+            else: 
                 print("Invalid Input")
 
             # Add an extra space between questions
