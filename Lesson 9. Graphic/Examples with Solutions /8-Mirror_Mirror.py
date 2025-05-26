a3a6087aaef8faf26c78b52c0c7d0fc3f89e9dcb
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-26 23:35:14 +0100

    Mirror_Mirror

--- Lesson 9. Graphic/Examples with Solutions /8-Mirror_Mirror.py
+++ Lesson 9. Graphic/Examples with Solutions /8-Mirror_Mirror.py
@@ -1,13 +1,13 @@
 """
-File: Mirror_Mirror.py
---------------------------
-This is a program that draws a blue rectangle on the other side of the canvas 
-which is the mirror image of the red rectangle. 
-(That is, the left edge of the blue rectangle is the same distance 
-from the line in the middle as the right edge of the red rectangle, 
-and the left edge of the red rectangle is the same distance 
-from the left edge of the canvas as the right edge of the blue rectangle is 
-from the right edge of the canvas!). 
+    File: Mirror_Mirror.py
+    -----------------------
+    This is a program that draws a blue rectangle on the other side of the canvas 
+    which is the mirror image of the red rectangle. 
+    That is, the left edge of the blue rectangle is the same distance 
+    from the line in the middle as the right edge of the red rectangle, 
+    and the left edge of the red rectangle is the same distance 
+    from the left edge of the canvas as the right edge of the blue rectangle is 
+    from the right edge of the canvas. 
 """
 
 from graphics import Canvas
@@ -25,7 +25,7 @@ def main():
         CANVAS_WIDTH // 2, 
         CANVAS_HEIGHT)
     
-    # Create red rectangle
+    # Creating a red rectangle
     rect_left_x = 20
     rect_top_y = 50
     rect_width = 100
@@ -37,7 +37,13 @@ def main():
         rect_top_y + rect_height, 
         'red')
     
-    pass # Delete this line and write your code here! :)
+    # Creating a blue rectangle using the canvas's bounds and the red rectangle's coordinates as guides.
+    canvas.create_rectangle(
+        CANVAS_WIDTH - rect_left_x - rect_width,  # Start at right bound, move left rect_left_x, move left rect_width
+        rect_top_y,  # Same top_y as red rectangle
+        CANVAS_WIDTH - rect_left_x,  # Take our left_x from two lines above and add our rect_width to it
+        rect_top_y + rect_height,  # Same bottom_y as red rectangle
+        'blue')
 
 
 """
