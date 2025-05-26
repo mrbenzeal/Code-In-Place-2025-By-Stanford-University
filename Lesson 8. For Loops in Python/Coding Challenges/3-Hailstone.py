602c01b3473a7dc2c8577a106d9d35d570e5a16c
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-26 19:00:44 +0100

    Hailstone

---
+++ Lesson 8. For Loops in Python/Coding Challenges/3-Hailstone.py
@@ -0,0 +1,22 @@
+"""
+File: Hailstone.py
+-------------------
+This program writes a code that implements the following process.
+Have the user input a positive integer, call it n.
+If n is even, divide it by two.
+If n is odd, multiply it by three and add one.
+Continue this process until n is equal to one.
+"""
+
+def main():
+    # your code here
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == "__main__":
+    main()
