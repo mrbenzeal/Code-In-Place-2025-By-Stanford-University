d68d26c3b38df056f377357ca71ad59b3566a5a6
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-26 23:16:03 +0100

    Ring_Ring

--- Lesson 9. Graphic/Examples with Solutions /6-Ring_Ring.py
+++ Lesson 9. Graphic/Examples with Solutions /6-Ring_Ring.py
@@ -26,17 +26,32 @@ RING_WIDTH = 10
 
 def main():
     my_canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-    
-    # Calculate the top left corner position
-    left_x = OUTER_LEFT_X - OUTER_DIAMETER/2
-    top_y = OUTER_TOP_Y - OUTER_DIAMETER/2
-    
-    # Calculate the right and bottom of the square
-    right_x = left_x + OUTER_DIAMETER
-    bottom_y = top_y + OUTER_DIAMETER
 
-	# drawing a red square and storing it in a variable 'red_square'
-    my_canvas.create_oval(left_x, top_y, right_x, bottom_y, 'red')
+    # Getting the middle of the canvas
+    middle_x = CANVAS_WIDTH/2
+    middle_y = CANVAS_HEIGHT/2
+    
+    # Calculating the top left corner position of the outer red circle
+    left_x_1 = middle_x - OUTER_DIAMETER/2
+    top_y_1 = middle_y - OUTER_DIAMETER/2
+    
+    # Calculating the right and bottom of the outer red circle
+    right_x_1 = left_x_1 + OUTER_DIAMETER
+    bottom_y_1 = top_y_1 + OUTER_DIAMETER
+
+    # Calculating the top left corner position of the white inner circle
+    left_x_2 = OUTER_LEFT_X + RING_WIDTH
+    top_y_2 = OUTER_TOP_Y + RING_WIDTH
+    
+    # Calculating the right and bottom of the white inner circle
+    right_x_2 = OUTER_LEFT_X + OUTER_DIAMETER - RING_WIDTH
+    bottom_y_2 = OUTER_TOP_Y + OUTER_DIAMETER - RING_WIDTH
+
+	# drawing a red outer circle 
+    my_canvas.create_oval(left_x_1, top_y_1, right_x_1, bottom_y_1, 'red')
+
+    # drawing a white inner circle
+    my_canvas.create_oval(left_x_2, top_y_2, right_x_2, bottom_y_2, 'white')
     
     
 """
