045b0072cd5975e8e96b7994c4b68e016bcb859a
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-05-26 19:49:00 +0100

    Scene_With_Functions

---
+++ Lesson 10. Functions/Coding Challenges/0-Scene_With_Functions.py
@@ -0,0 +1,115 @@
+"""
+File: Scene_With_Functions.py
+------------------------------
+This program writes a code that .
+"""
+
+from graphics import Canvas
+import math
+    
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 300
+
+CLOUD_WIDTH = 120
+CLOUD_HEIGHT = 80
+
+TRUNK_HEIGHT = 80
+TRUNK_WIDTH = 20
+LEAVES_SIZE = 60
+
+TREE_BOTTOM_Y = CANVAS_HEIGHT - 20 
+
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    draw_cloud_1(canvas, 140, 10, 'salmon')
+    # drawing two more clouds, and three trees
+    draw_cloud_2(canvas, 140, 10, 'salmon')
+    draw_cloud_3(canvas, 140, 10, 'salmon')
+    draw_tree_1(canvas, 140, 10, 'salmon')
+    draw_tree_2(canvas, 140, 10, 'salmon')
+    draw_tree_3(canvas, 140, 10, 'salmon')
+
+
+def draw_cloud_1(canvas, x, y, color):
+    """
+    This function draws one cloud. You can call it and pass in 
+    different values of x and y (the location of the cloud) and
+    color (the color of the cloud). 
+    """
+    cloud_bottom_start_y = y + (1/3) * CLOUD_HEIGHT
+    cloud_bottom_end_y = y + CLOUD_HEIGHT
+    cloud_top_start_x = x + (1/4) * CLOUD_WIDTH
+    cloud_top_end_x = x + (3/4) * CLOUD_WIDTH
+
+    # Bottom two puffs
+    canvas.create_oval(
+        x, 
+        cloud_bottom_start_y,
+        x + (3/4) * CLOUD_WIDTH,
+        cloud_bottom_end_y,
+        color
+    )
+    canvas.create_oval(
+        x + (1/4) * CLOUD_WIDTH, 
+        cloud_bottom_start_y,
+        x + CLOUD_WIDTH,
+        cloud_bottom_end_y,
+        color
+    )
+
+    # Top puff
+    canvas.create_oval(
+        cloud_top_start_x,
+        y,
+        cloud_top_end_x,
+        y + (2/3) * CLOUD_HEIGHT,
+        color
+    )
+    """
+    This function draws one cloud. You can call it and pass in 
+    different values of x and y (the location of the cloud) and
+    color (the color of the cloud). 
+    """
+    cloud_bottom_start_y = y + (1/3) * CLOUD_HEIGHT
+    cloud_bottom_end_y = y + CLOUD_HEIGHT
+    cloud_top_start_x = x + (1/4) * CLOUD_WIDTH
+    cloud_top_end_x = x + (3/4) * CLOUD_WIDTH
+
+    # Bottom two puffs
+    canvas.create_oval(
+        x, 
+        cloud_bottom_start_y,
+        x + (3/4) * CLOUD_WIDTH,
+        cloud_bottom_end_y,
+        color
+    )
+    canvas.create_oval(
+        x + (1/4) * CLOUD_WIDTH, 
+        cloud_bottom_start_y,
+        x + CLOUD_WIDTH,
+        cloud_bottom_end_y,
+        color
+    )
+
+    # Top puff
+    canvas.create_oval(
+        cloud_top_start_x,
+        y,
+        cloud_top_end_x,
+        y + (2/3) * CLOUD_HEIGHT,
+        color
+    )
+
+# TODO: You should define a function like draw_cloud
+# for trees, as well as for any extra elements in the scene.
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == '__main__':
+    main() 
