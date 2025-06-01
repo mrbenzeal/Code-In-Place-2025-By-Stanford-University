c2e89569469c31c7af39cf94cd282dc4a974ba31
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-01 23:49:02 +0100

    Even_Odd_Numbers

--- CIP-2025 Diagnostic Exams/Even_Odd_Numbers.py
+++ CIP-2025 Diagnostic Exams/Even_Odd_Numbers.py
@@ -1,3 +1,18 @@
+"""
+DIAGNOSTIC_EXAM
+----------------
+File: Even_Odd_Numbers.py
+---------------------------
+Print out each of the numbers 1 through 100 and whether that number is even or odd.
+100 is specified using a constant MAX_NUMBER.
+Here is what the output looks like when MAX_NUMBER = 100
+1 is odd
+2 is even
+...
+99 is odd
+100 is even
+"""
+
 # print numbers from 1 up until MAX_NUMBER, inclusive
 MAX_NUMBER = 100
 
