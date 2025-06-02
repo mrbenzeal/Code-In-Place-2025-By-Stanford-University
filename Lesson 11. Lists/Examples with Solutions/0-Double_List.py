55235d35eff49eefad1ebc131251cd26b528d615
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-02 23:26:31 +0100

    Double_List

--- Lesson 11. Lists/Examples with Solutions/0-Double_List.py
+++ Lesson 11. Lists/Examples with Solutions/0-Double_List.py
@@ -16,11 +16,14 @@ def main():
         elem_at_index = numbers[i]  # Get the element at index i in the numbers list
         numbers[i] = elem_at_index * 2  # Set the element at index i to be equal to the previous element times 2
     """
-    
     print(numbers) # This should print the doubled list
 
 
-# There is no need to edit code beyond this point
-
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == '__main__':
     main()
