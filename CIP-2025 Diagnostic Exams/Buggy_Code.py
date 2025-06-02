4f1da22e7545cbf2abb5dd4824108398c21582c3
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-02 01:13:48 +0100

    Buggy_Code

--- CIP-2025 Diagnostic Exams/Buggy_Code.py
+++ CIP-2025 Diagnostic Exams/Buggy_Code.py
@@ -1,3 +1,43 @@
+"""
+DIAGNOSTIC_EXAM
+----------------
+File: Buggy_Code.py
+---------------------
+Consider the following buggy code:
+
+from graphics import Canvas
+
+def main():
+    # draws two cars
+    canvas = Canvas(400, 400)
+    x = 10
+    y = 10
+    draw_car()
+
+    x = 100
+    y = 100
+    draw_car()
+
+def draw_car():
+    # draws a car at the location x, y
+    # you can assume that math offsets for the rectangles are correct
+    canvas.create_rectangle(x, y, x + 50, y + 20)
+    canvas.create_rectangle(x + 10, y - 10, x + 40, y + 20)
+
+if __name__ == '__main__':
+    main()
+
+The programmer wants to draw two cars, one at location (10, 10) and another at location (100, 100). 
+When they run their program they get a "NameError" 
+and the IDE complains that inside draw_car it doesn't know what canvas, x, or y mean.
+
+Fix this program so that the location information is correctly given to draw_car. 
+You can make changes to both draw_car and main. Write a comments for each line you changed.
+
+Note that you should assume that the offsets in draw_car are correct. 
+You are not meant to be worrying about the canvas coordinates, rather the control flow of the program.
+"""
+
 from graphics import Canvas
 
 def main():
