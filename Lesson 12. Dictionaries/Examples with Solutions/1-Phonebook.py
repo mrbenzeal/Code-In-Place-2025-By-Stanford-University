7145cee096166cae208bf1705e9bc927d5acd8fd
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-03 02:03:04 +0100

    Phonebook

---
+++ Lesson 12. Dictionaries/Examples with Solutions/1-Phonebook.py
@@ -0,0 +1,69 @@
+"""
+File: Phonebook.py
+-------------------
+This program shows an example of using dictionaries 
+to keep track of information in a phonebook.
+"""
+
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+
+Note: The starter code for this example is the solution.
+"""
+
+def read_phone_numbers():
+    """
+    Ask the user for names/numbers to story in a phonebook (dictionary).
+    Returns the phonebook.
+    """
+    phonebook = {} # Create empty phonebook
+
+    while True:
+        name = input("Name: ")
+        if name == "":
+            break
+        number = input("Number: ")
+        phonebook[name] = number
+
+    return phonebook
+
+
+def print_phonebook(phonebook):
+    """
+    Prints out all the names/numbers in the phonebook.
+    """
+    for name in phonebook:
+        print(str(name) + " -> " + str(phonebook[name]))
+
+
+def lookup_numbers(phonebook):
+    """
+    Allow the user to lookup phone numbers in the phonebook
+    by looking up the number associated with a name.
+    """
+    while True:
+        name = input("Enter name to lookup: ")
+        if name == "":
+            break
+        if name not in phonebook:
+            print(name + " is not in the phonebook")
+        else:
+            print(phonebook[name])
+
+
+def main():
+    phonebook = read_phone_numbers()
+    print_phonebook(phonebook)
+    lookup_numbers(phonebook)
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == '__main__':
+    main()
