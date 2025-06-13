733d6c3203dc7ec6c9543278873bb8e6863b4c3f
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-13 17:17:32 +0100

    Breakout

---
+++ My CIP Projects/5-Breakout.py
@@ -0,0 +1,24 @@
+from graphics import Canvas
+import time
+import random
+import math
+
+CANVAS_WIDTH = 500
+CANVAS_HEIGHT = 600
+PADDLE_Y = CANVAS_HEIGHT - 30
+PADDLE_WIDTH = 80
+PADDLE_HEIGHT = 15
+BALL_RADIUS = 10
+
+BRICK_GAP = 5
+BRICK_WIDTH = (CANVAS_WIDTH - BRICK_GAP*9) / 10
+BRICK_HEIGHT = 10
+
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    # TODO: your code here
+    
+
+if __name__ == '__main__':
+    main()
