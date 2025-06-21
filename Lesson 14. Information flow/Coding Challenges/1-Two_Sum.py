19da3432d8b6e865e5bf0bae77bf14b667c9f3a5
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-21 23:46:28 +0100

    Two_Sum

---
+++ Lesson 14. Information flow/Coding Challenges/1-Two_Sum.py
@@ -0,0 +1,43 @@
+"""
+File: Two_Sum.py
+------------------
+This is a program 
+"""
+
+def main():
+    """
+    Calls two_sum_finder on a few sample inputs.
+    You can add more test cases here to check your work!
+    """
+    print(two_sum([2, 7, 11, 15], 9))     # Expected: True
+    print(two_sum([1, 2, 3, 4], 8))       # Expected: False
+    print(two_sum([5, 5], 10))            # Expected: True
+    print(two_sum([4], 8))                # Expected: False
+
+
+def two_sum(nums, target):
+    """
+    Returns True if any two distinct elements in the list `nums`
+    add up to the value `target`. Otherwise, returns False.
+
+    Examples:
+    two_sum([2, 7, 11, 15], 9) -> True
+    two_sum([1, 2, 3, 4], 8) -> False
+    """
+    seen = set()
+    for num in nums:
+        complement = target - num
+        if complement in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+"""
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+if __name__ == '__main__':
+    main()
