a651c47d91a71ddb8ff26cc4b6539d6d46acf678
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-21 17:50:18 +0100

    Follow_Me

--- Lesson 13. Animation/Coding Challenges/1-Follow_Me.py
+++ Lesson 13. Animation/Coding Challenges/1-Follow_Me.py
@@ -41,6 +41,5 @@ def main():
 # It allows the script to be run as the main program or imported 
   without executing the main function immediately.
 """
-# There is no need to edit code beyond this point
 if __name__ == '__main__':
     main()
