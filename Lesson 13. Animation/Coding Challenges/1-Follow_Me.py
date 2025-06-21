fab3be4ac49c9daf868e4fd31716dfa309711110
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-21 17:47:26 +0100

    Follow_Me

---
+++ Lesson 13. Animation/Coding Challenges/1-Follow_Me.py
@@ -0,0 +1,46 @@
+"""
+File: Follow_Me.py
+-------------------
+This is a program 
+"""
+
+from graphics import Canvas
+import time
+
+CANVAS_SIZE = 400
+BALL_DIAMETER = 50
+PAUSE_TIME = 1/50
+
+def main():
+    canvas = Canvas(CANVAS_SIZE, CANVAS_SIZE)
+    ball = canvas.create_oval(
+        0, 0,
+        BALL_DIAMETER, 
+        BALL_DIAMETER,
+        'blue'
+    )
+    
+    radius = BALL_DIAMETER // 2
+
+    while True:
+        mouse_x = canvas.get_mouse_x()
+        mouse_y = canvas.get_mouse_y()
+        
+        # Center the ball on the mouse position
+        new_x = mouse_x - radius
+        new_y = mouse_y - radius
+        canvas.moveto(ball, new_x, new_y)
+        
+        time.sleep(PAUSE_TIME)
+        print("Mouse location: (" + str(mouse_x) + ", " + str(mouse_y) + ")")
+
+
+"""
+# This provided line is required at the end of the Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
+# There is no need to edit code beyond this point
+if __name__ == '__main__':
+    main()
