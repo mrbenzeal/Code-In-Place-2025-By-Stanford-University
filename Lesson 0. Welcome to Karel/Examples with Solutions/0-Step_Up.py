a34b0d0aa6d2f59487a0a5c41e297a285c4307e9
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-24 08:14:20 +0100

    Step_Up

--- Lesson 0. Welcome to Karel/Examples with Solutions/0-Step_Up.py
+++ Lesson 0. Welcome to Karel/Examples with Solutions/0-Step_Up.py
@@ -1,4 +1,10 @@
-# This tells python who Karel is!
+"""
+File: Step_Up.py
+-----------------
+This program has Karel pick up the beeper and put it on the ledge.
+"""
+
+# This tells PyCharm who Karel is
 # Every Karel file has a line just like it
 from karel.stanfordkarel import *
 
@@ -16,11 +22,17 @@ def main():
 
 # the definition of turn_right (Karel turn right by turning left three times)
 def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+    for i in range(3):
+        turn_left()
+    
 
+"""
 # This is "boilerplate" code which launches your code
-# when you hit the run button
+  when you hit the run button
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == '__main__':
     main()
