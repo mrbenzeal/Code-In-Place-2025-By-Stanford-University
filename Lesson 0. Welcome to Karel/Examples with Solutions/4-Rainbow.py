e83e0f5cef4d93a982c8a0b1bf75bcd0a1040e5c
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-24 08:40:27 +0100

    Rainbow

--- Lesson 0. Welcome to Karel/Examples with Solutions/4-Rainbow.py
+++ Lesson 0. Welcome to Karel/Examples with Solutions/4-Rainbow.py
@@ -1,16 +1,22 @@
 """
 File: Rainbow.py
-----------------------
-Karel makes a rainbow!
+-----------------
+This program has Karel paint a rainbow!
+What colors does Karel know? So many! 
+In fact any "html" color will work. 
+Here is a link with a few:
+
+https://htmlcolorcodes.com/color-names/
 """
+
 from karel.stanfordkarel import *
 
 def main():
     """
-    You should write your code to make Karel do its task in
-    this function. Make sure to delete the 'pass' line before
-    starting to write your own code. You should also delete this
-    comment and replace it with a better, more descriptive one.
+    Karel starts in the left corner of a world with 1 row and 6 columns, 
+    Karel paints the squares with, in order: 
+    the colors "red" , "orange", "yellow", "green", and "blue", 
+    and then Karel moves to end in the rightmost spot.
     """
     paint_corner('red')
     move()
@@ -24,7 +30,14 @@ def main():
     move()
     
 
-# There is no need to edit code beyond this point
-
+"""
+# This is "boilerplate" code which launches your code
+  when you hit the run button
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == '__main__':
     main()
+    
