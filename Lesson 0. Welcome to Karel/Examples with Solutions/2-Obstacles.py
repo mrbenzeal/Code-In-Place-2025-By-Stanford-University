bcb4900e2cd37e18ccf277974a5a36c3071b003f
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-24 08:26:05 +0100

    Obstacles

--- Lesson 0. Welcome to Karel/Examples with Solutions/2-Obstacles.py
+++ Lesson 0. Welcome to Karel/Examples with Solutions/2-Obstacles.py
@@ -1,8 +1,17 @@
+"""
+File: Obstacles.py
+-------------------
+This program gets Karel over the obstacles, 
+puts beepers in the squares directly to the right of the walls, 
+and moves Karel to the bottom right corner of the world.
+"""
+
 from karel.stanfordkarel import *
 
 def main():
     """
-    Karel starts facing East in the bottom left corner of the world and ends facing East in the bottom right corner of the world.
+    Karel starts facing East in the bottom left corner of the world 
+    and ends facing East in the bottom right corner of the world.
     """
     move()
     turn_left()
@@ -20,11 +29,12 @@ def main():
     move()
     move()
 
+
 # the definition of turn_right (Karel turn right by turning left three times)
 def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+    for i in range(3):
+        turn_left()
+    
 
 # this definition gets Karel into the obstacle walls, put the beeper and leave 
 def go_in_put_beeper_and_leave():
@@ -37,7 +47,13 @@ def go_in_put_beeper_and_leave():
     turn_right()
 
 
-# There is no need to edit code beyond this point
-
+"""
+# This is "boilerplate" code which launches your code
+  when you hit the run button
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == '__main__':
     main()
