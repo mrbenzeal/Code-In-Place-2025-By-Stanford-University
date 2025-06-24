4264822d76f8f4ee7b516deb2b5ba6eeb70c688b
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-24 08:53:43 +0100

    Example_Code_2

--- Lesson 1. For Loops/Examples with Solutions/1-Example_Code_2.py
+++ Lesson 1. For Loops/Examples with Solutions/1-Example_Code_2.py
@@ -14,7 +14,14 @@ def main():
         put_beeper()
         turn_left()
 
+
+"""
 # This is "boilerplate" code which launches your code
-# when you hit the run button
+  when you hit the run button
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == '__main__':
     main()
