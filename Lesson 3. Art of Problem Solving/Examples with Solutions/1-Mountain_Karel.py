5ee4a49a2168a49605b5f96ac47b941a115f18c9
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-26 00:20:57 +0100

    Mountain_Karel

--- Lesson 3. Art of Problem Solving/Examples with Solutions/1-Mountain_Karel.py
+++ Lesson 3. Art of Problem Solving/Examples with Solutions/1-Mountain_Karel.py
@@ -1,23 +1,19 @@
-# This tells Python who Karel is
-# Every Karel file has a line just like it
+"""
+File: Mountain_Karel.py
+------------------------
+This program has Karel climb a mountain of any size
+and plants a beeper at the top.
+"""
+
 from karel.stanfordkarel import *
 
-"""
-# The mountain karel example from the video!
-This program instructs karel to climb up the mountain top, 
-plant a flag (beeper) 
-and climb down the mountain.
-"""
-
 def main():
-    # go up the mountain, place flag and come down the mountain.
-    climb_up_mountain()
-    plant_flag()
-    climb_down_mountain()
+    climb_mountain()
+    put_beeper()
+    descend_mountain()
     
 
-#A
-def climb_up_mountain():
+def climb_mountain():
     """
     Pre: Karel is at the base of a mountain
     Post: Karel is standing at the top.
@@ -27,44 +23,18 @@ def climb_up_mountain():
     while front_is_blocked():
         step_up()
         
-
-#A1
-def step_up():
-    # Karel takes one step up the mountain
-    # In both Pre/Post condition, Karel is facing right (East)
-    turn_left()
-    move()
-    turn_right()
-    move()
-
-
-#A1a
-def turn_right():
-    # Makes karel turn in the clock-wise direction.
-    # Works in any pre-condition state!
-    for i in range(3):
-        turn_left()
-
-
-#B
-def plant_flag():
-    # a beeper, the national flag of karel land
-    put_beeper()
-
-
-#C
-def climb_down_mountain():
+        
+def descend_mountain():
     """
-    Pre: Karel is at the top of the mountain, facing right
-    Post: Karel is at the bottom (base) of the mountain, facing right
+    Pre: Karel is at the top of a mountain
+    Post: Karel is on the other side
     Karel's front will only be blocked when
     Karel is at the base, facing the far right wall
     """
     while front_is_clear():
         step_down()
-    
 
-#C1
+
 def step_down():
     """
     Take one step down. In both Pre/Post Karel is 
@@ -74,9 +44,35 @@ def step_down():
     turn_right()
     move()
     turn_left()
+    
+    
+def step_up():
+    """
+    Take one step up. In both Pre/Post Karel is 
+    facing right (East)
+    """
+    turn_left()
+    move()
+    turn_right()
+    move()
 
 
+def turn_right():
+    """
+    Makes karel turn in the clock-wise direction.
+    Works in any pre-condition state!
+    """
+    for i in range(3):
+        turn_left()
+
+
+"""
 # This is "boilerplate" code which launches your code
-# when you hit the run button
+  when you hit the run button
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == '__main__':
     main()
