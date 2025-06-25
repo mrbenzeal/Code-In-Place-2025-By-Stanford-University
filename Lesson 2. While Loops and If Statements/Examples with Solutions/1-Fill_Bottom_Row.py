3f8dcd3dbc0ad3d4ed18c6815af4947f90211a37
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2025-06-25 23:38:43 +0100

    Fill_Bottom_Row

--- Lesson 2. While Loops and If Statements/Examples with Solutions/1-Fill_Bottom_Row.py
+++ Lesson 2. While Loops and If Statements/Examples with Solutions/1-Fill_Bottom_Row.py
@@ -1,6 +1,7 @@
 """
-This is a worked example. It shows an example of Karel
-using a while loop to place a row of beepers
+File: Fill_Bottom_Row.py
+-------------------------
+This program has Karel use a while loop to place a row of beepers.
 """
 
 from karel.stanfordkarel import *
@@ -12,10 +13,17 @@ def main():
     while front_is_clear():
         put_beeper()
         move()
-    # this is a "fence-post" or an "off by one error" bug correction
+    # this is a fence-post bug correction
     put_beeper()
 
 
-# There is no need to edit code beyond this point
+"""
+# This is "boilerplate" code which launches your code
+  when you hit the run button
+# This provided line is required at the end of Python file 
+  to call the main() function.
+# It allows the script to be run as the main program or imported 
+  without executing the main function immediately.
+"""
 if __name__ == '__main__':
     main()
